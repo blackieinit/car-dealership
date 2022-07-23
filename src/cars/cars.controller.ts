d0bcbfc42b853e0e740c03daa91ca73425f2f2f5
Daniel Parra <danielparra@encodingtech.com>
2022-07-23 12:31:24 -0400

    [ADD] Controllers

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+    private cars = ['Toyota', 'Honda', 'Jeep'];
     
     @Get()
-    getAllCars(){
-        return ['Toyota', 'Honda', 'Jeep']
+    getAllCars() {
+        return this.cars;
     }
-    
+
+    @Get(':id')
+    getCarById( @Param('id') id: number ) {
+
+        return this.cars[id]
+
+    }
 }
