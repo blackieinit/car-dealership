a5ad076052682496458ed6979e67006b7b492597
Daniel Parra <danielparra@encodingtech.com>
2022-07-28 22:26:32 -0400

    [PIPES]

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -20,8 +21,8 @@ export class CarsController {
     }
 
     @Post()
-    create( @Body() body:any) {
-        return body
+    create( @Body() createCarDto: CreateCarDto) {
+        return createCarDto;
     }
 
     @Patch(':id')
